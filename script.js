399bbd7e29fe2b612bc581fe700deb43cdeae5ca
Hadi <104451011+khanhadi@users.noreply.github.com>
2023-03-01 15:52:49 +0300

    Set up barebones calculator and functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// CALC FUNCTIONS
+function add(x, y) {
+	return x + y;
+}
+
+function subtract(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
+
+function operate(x, y, operand) {
+	return operand(x, y);
+}
